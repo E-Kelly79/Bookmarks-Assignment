f649b039aadb689fed335fe5e46f5a628cd99764
Glitch (ahead-hiss) <none>
2018-03-07 23:04:33 +0000

    ❣️💏 Checkpoint package.json:641206/15 server.js:641206/52 public/styles.scss:641206/101 views/layouts/main.hbs:641206/6 views/partials/bookmarks.hbs:641206/11

--- server.js
+++ server.js
@@ -7,15 +7,19 @@ var assets = require("./assets");
 const app = express();
 var sassMiddleware = require("node-sass-middleware");
 
+app.use("/assets", assets);
+
 app.use(sassMiddleware({
-  src: __dirname + '/public/',
-  dest: '/temp'
+  src: __dirname + '/public',
+  dest: '/tmp'
 }));
 
-app.use("/assets", assets);
+
 const exphbs = require('express-handlebars');
 app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(express.static('public'));
+app.use(express.static('/tmp'));
+
 app.engine('.hbs', exphbs({
   extname: '.hbs',
   defaultLayout: 'main',
