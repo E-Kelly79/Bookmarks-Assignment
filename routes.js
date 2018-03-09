721850435d3c3c9483a8626bc434b2f443923c5d
Glitch (eoinkellywebassignment) <none>
2018-03-09 22:25:30 +0000

    📼💐 Checkpoint server.js:641206/3 routes.js:641206/43

--- routes.js
+++ routes.js
@@ -9,11 +9,11 @@ const about     =require('./controllers/about.js');
 const bookmark  =require('./controllers/bookmark.js');
 
 router.get('/', start.index);
-router.get('/about', about.index);
 router.get('/dashboard', dashboard.index);
 router.get('/bookmark/:id', bookmark.index);
 router.get('/bookmark/:id/deletelink/:linkid', bookmark.deleteLink);
 router.get('/dashboard/deletebookmark/:id', dashboard.deleteBookmark);
+router.get('/about', about.index);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -23,15 +23,6 @@ app.use(express.static('/tmp'));
 app.engine('.hbs', exphbs({
   extname: '.hbs',
   defaultLayout: 'main',
-  // helpers: {
-  //   CalculateDuration: function (songs) {
-  //      let total=0;
-  //      for (let i in songs) {
-  //         total+=songs[i].duration;
-  //      }
-  //      return total + ' mins';
-  //   }
-  // }
 }));
 app.set('view engine', '.hbs');
 
