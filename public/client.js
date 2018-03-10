5b6ffbe788405609afdd15296f1e553d0c328282
Glitch (eoinkelly) <none>
2018-03-10 10:53:15 +0000

    ⏳🖇 Checkpoint public/client.js:641206/144 views/partials/bookmarks.hbs:641206/20 views/about.hbs:641206/171 public/styles.scss:641206/23

--- public/client.js
+++ public/client.js
@@ -1,19 +1,9 @@
 
 $(".dellink").click(function() {
- return bootbox.confirm({
-    message: "This is a confirm with custom button text and color! Do you like it?",
-    buttons: {
-        confirm: {
-            label: 'Yes',
-            className: 'btn-success'
-        },
-        cancel: {
-            label: 'No',
-            className: 'btn-danger'
-        }
-    },
-    callback: function (result) {
-        console.log('This was logged in the callback: ' + result);
-    }
+ return confirm('Really delete this bookmark?');
 });
+
+$(".delete-bookmarks").click(function() {
+  return confirm('Really delete this folder?');
 });
+
