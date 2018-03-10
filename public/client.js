9978231fc9007047bee7958c538db8c1c86548af
Glitch (eoinkelly) <none>
2018-03-10 10:42:15 +0000

    💁🙆 Checkpoint views/layouts/main.hbs:641206/90 public/client.js:641206/222

--- public/client.js
+++ public/client.js
@@ -1,4 +1,19 @@
 
 $(".dellink").click(function() {
-  return confirm('Really delete this song?');
+ return bootbox.confirm({
+    message: "This is a confirm with custom button text and color! Do you like it?",
+    buttons: {
+        confirm: {
+            label: 'Yes',
+            className: 'btn-success'
+        },
+        cancel: {
+            label: 'No',
+            className: 'btn-danger'
+        }
+    },
+    callback: function (result) {
+        console.log('This was logged in the callback: ' + result);
+    }
+});
 });
